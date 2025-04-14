c4914d11f6e458f1166bb36ab3f43bfea29b83d1
amit redhu <redhus341@gmail.com>
2025-04-14 05:39:04 +0530

    change in docker.sh

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -2,9 +2,11 @@
 # Log everything to start_docker.log
 exec > /home/ubuntu/start_docker.log 2>&1
 
+# Export CI_TOKEN (set your token here)
+export CI_TOKEN=94f5f599e01d773416c043c7b7e819b789f3b7b8
+
 echo "Logging in to ECR..."
-aws ecr get-login-password --region ap-southeast-2 | \
-docker login --username AWS --password-stdin 565393027942.dkr.ecr.ap-southeast-2.amazonaws.com
+aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 565393027942.dkr.ecr.ap-southeast-2.amazonaws.com
 
 echo "Pulling Docker image..."
 docker pull 565393027942.dkr.ecr.ap-southeast-2.amazonaws.com/swiggy-food-delivery-time-prediction:latest
@@ -21,8 +23,5 @@ if [ "$(docker ps -aq -f name=swiggy-food-delivery-time-prediction)" ]; then
 fi
 
 echo "Starting new container..."
-docker run -d -p 80:8000 --name swiggy-food-delivery-time-prediction \
--e DAGSHUB_USER_TOKEN=94f5f599e01d773416c043c7b7e819b789f3b7b8 \
-565393027942.dkr.ecr.ap-southeast-2.amazonaws.com/swiggy-food-delivery-time-prediction:latest
-
+docker run -d -p 80:8000 --name swiggy-food-delivery-time-prediction -e DAGSHUB_USER_TOKEN=$CI_TOKEN 565393027942.dkr.ecr.ap-southeast-2.amazonaws.com/swiggy-food-delivery-time-prediction:latest
 echo "Container started successfully."
